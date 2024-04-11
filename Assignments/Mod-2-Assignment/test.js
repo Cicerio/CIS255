ae436aa59ad5045c42c7f6f2eabc74313ac0d758
Ralph L Robinson <ralphrobinson@student.olympic.edu>
2024-04-11 15:05:47 -0700

    Skills demo 2

---
+++ Assignments/Mod-2-Assignment/test.js
@@ -0,0 +1,11 @@
+/* 3 Examples of basic arithmetic in JS */
+console.log(10/3); // Output: 3.3333333333333335
+console.log(10%3); // Output: 1
+console.log(10**3); // Output: 1000
+/* 2 Examples of declaring variables in JS */
+var x = "10";
+let y=10, z=20, v=30;
+/* 2 Examples of using variables in arithmetic operations in JS */
+console.log(y/z); // Output: 0.5
+console.log(x + v); //concatenation, Output: 1030
+console.log(z+y+v); // Output: 60
